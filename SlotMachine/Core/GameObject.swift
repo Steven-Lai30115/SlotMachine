1f4f90098126772ec7b4af9062ca002a3ababd3f
Steven Lai <wai30115@live.hk>
2023-01-17 11:40:26 -0500

    Added GameObject class

--- SlotMachine/Core/GameObject.swift
+++ SlotMachine/Core/GameObject.swift
@@ -1,8 +1,25 @@
-//
-//  GameObject.swift
-//  SlotMachine
-//
-//  Created by chin wai lai on 17/1/2023.
-//
+import GameplayKit
+import SpriteKit
 
-import Foundation
+class GameObject : SKSpriteNode{
+    var scale : CGFloat?
+    var width  : CGFloat?
+    var height : CGFloat?
+    
+    init(imageString: String, initialScale : CGFloat){
+        let texture = SKTexture(imageNamed: imageString)
+        let color = UIColor.clear
+        super.init(texture: texture, color: color, size: texture.size())
+        
+        scale = initialScale
+        setScale(scale!)
+        width = texture.size().width * scale!
+        height = texture.size().height * scale!
+        name = imageString
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
