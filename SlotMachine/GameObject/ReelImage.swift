f8b9d3b1eab5008ff1ba5ab52447b3fc0e537588
Man Nok Pun <sgloriasca@gmail.com>
2023-01-27 17:08:34 -0500

    Fix Reel image reset bug

--- SlotMachine/GameObject/ReelImage.swift
+++ SlotMachine/GameObject/ReelImage.swift
@@ -9,12 +9,14 @@ class ReelImage : UIElement{
     var bottomBoundary: CGFloat?
     let margin = 16.0
     var spinSpeed = 20.0
+    var tempY: CGFloat?
     
     init(imageString : String, scale : CGFloat, _zPosition: CGFloat, _reel: Reel, _index: Int8){
         super.init(_name: "\(imageString)\(String(describing: _reel.index))\(_index)", imageString: imageString, initialScale: scale, _zPosition: _zPosition)
         reel = _reel
         index = _index
         height = _reel.height
+        halfHeight = _reel.halfHeight
         
     }
     
@@ -33,21 +35,28 @@ class ReelImage : UIElement{
     }
     
     func resetPositionY() {
-        position.y = reel!.position.y + height! * CGFloat(reel!.numberOfSpin!-2) + reel!.halfHeight!
+         position.y = position.y + reel!.height! * CGFloat(reel!.numberOfSpin!)
     }
     
     func setFinalPosition() {
-        if( position.y < reel!.position.y + reel!.halfHeight!
-            && position.y > reel!.position.y - reel!.halfHeight! ){
+        if( position.y <= reel!.position.y + ceil(reel!.halfHeight!)
+            && position.y > reel!.position.y - ceil(reel!.halfHeight!) ){
+            tempY = position.y
             position.y = reel!.position.y
             isHidden = false
             reel!.setSpinResult(_spinResult: self)
-        } else { isHidden = true }
+        } else { isHidden = true
+            tempY = nil
+        }
     }
     
     func spin(){
+        if( tempY != nil ) {
+            position.y = tempY!
+            tempY = nil
+        }
         position.y -= spinSpeed
-        if ( position.y <= reel!.position.y - reel!.height! - reel!.halfHeight! - margin)  {
+        if ( position.y <= reel!.position.y - reel!.height! )  {
             resetPositionY()
         }
         else if( position.y <= reel!.position.y - reel!.halfHeight! + margin) {
